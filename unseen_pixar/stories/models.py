c3c7355459b5feb8815360fd5d3f7b41d066c737
Connor Hartwell <c.hartwell@newcastle.ac.uk>
2020-06-05 19:07:44 +0100

    added basic database plan

---
+++ unseen_pixar/stories/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+#base story
+#storyid - genre - ouat - everyday - oneday - result - result2 - fin
+
+#genre - each story must have a genre. can be a concept instead of a genre. helps the user get imagination flowing.
+#genreid - description
+
+#usertable
+#userid - username - email
+
+#userstories ( stops users from working on same story twice, and can track when a user's story is complete. )
+#userid - storyid
+
+# Create your models here.
