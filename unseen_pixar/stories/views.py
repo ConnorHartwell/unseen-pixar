7730c7ccd0c69044a0053d05986efa5f17183536
Connor Hartwell <c.hartwell@newcastle.ac.uk>
2020-06-08 21:37:12 +0100

    lots of random changes

--- unseen_pixar/stories/views.py
+++ unseen_pixar/stories/views.py
@@ -1,19 +1,33 @@
 from django.shortcuts import render
 
-from django.http import HttpRequest,Http404
-from .story import get_active_story
+from django.http import Http404, HttpRequest, HttpResponse
+from .story import *
+from django.template import Context
 
 def index(request):
     story = get_active_story()
     if(story != None):
         context = {
-                    'story' : story.once_upon_a_time,
-                    'stage' : 'Once upon a time',
-                    'story-id' : story.pk
+                    'this_story' : story,
+                    'stageid' : '0',
                 }
     else:
         context = {
+            'stage' : '',
             'story' : "Once upon a time, there was ..."
         }
 
     return render(request, 'stories/index.html',context)
+
+def get_story_from_id(request, storyid):
+    content = Context({
+        'story' : Story.objects.get(pk=storyid),
+        'stage' : 0,
+    })
+    return render(request, 'stories/base.html',content)
+
+def get_curr_story_text(storyid, stageid):
+    return HttpResponse(get_curr_story_text(storyid,stageid))
+
+def get_curr_story_header(stageid):
+    return HttpResponse(stage_id_to_string(stageid))
